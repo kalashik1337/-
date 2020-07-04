15547fe76463ea8c68ca48bbf8dbab9bff2fe709
kalashik1337 <55557075+kalashik1337@users.noreply.github.com>
2020-07-04 09:59:01 +0300

    Add files via upload

---
+++ Абстрактные классы.cpp
@@ -0,0 +1,28 @@
+﻿class A {
+    static int counter;
+    int my_number;
+public:
+    A()
+    {
+        my_number = counter;
+        counter++;
+    }
+    int get_number() const
+    {
+        return my_number;
+    }
+};
+int A::counter = 0;
+int static_counter_of_instances()
+{
+    A a, b, c, d, e;
+    A x[10];
+    cout << "a number " << a.get_number() << endl;
+    cout << "b number " << b.get_number() << endl;
+    cout << "c number " << c.get_number() << endl;
+    cout << "d number " << d.get_number() << endl;
+    cout << "e number " << e.get_number() << endl;
+    for (int i = 0; i < 10; i++)
+        cout << "x[" << i << "] number " << x[i].get_number() << endl;
+    return 0;
+}
---
+++ динамический полиморфизм.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+class Figure
+{
+public:
+    virtual double getSquare() = 0;
+    virtual double getPerimeter() = 0;
+    virtual void showFigureType() = 0;
+};
+class Rectangle : public Figure
+{
+private:
+    double width;
+    double height;
+public:
+    Rectangle(double w, double h) : width(w), height(h)
+    {
+    }
+    double getSquare() override
+    {
+        return width * height;
+    }
+    double getPerimeter() override
+    {
+        return width * 2 + height * 2;
+    }
+    void showFigureType()
+    {
+        std::cout << "Rectangle" << std::endl;
+    }
+};
+class Circle : public Figure
+{
+private:
+    double radius;
+public:
+    Circle(double r) : radius(r)
+    {
+    }
+    double getSquare() override
+    {
+        return radius * radius * 3.14;
+    }
+    double getPerimeter() override
+    {
+        return 2 * 3.14 * radius;
+    }
+    void showFigureType()
+    {
+        std::cout << "Circle" << std::endl;
+    }
+};
+
+int main()
+{
+    Rectangle rect(30, 50);
+    Circle circle(30);
+
+    std::cout << "Rectangle square: " << rect.getSquare() << std::endl;
+    std::cout << "Circle square: " << circle.getSquare() << std::endl;
+
+    return 0;
+}
